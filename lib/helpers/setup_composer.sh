a101864af7a66c827a97571183069e15cb48ccbb
Daniel Koch <dkoch@westpfalz-klinikum.de>
2019-01-21 12:10:52 +0100

    setup_composer: remove -q arg from wget
    I just tried to install passbolt CE and this script failed silently
    e.g.
    
    ======================
    Installing composer...
    ======================
    EXIT
    
    It failed because $https_proxy was not set.
    Without the -q arg i would have got useful information why the script
    stopped. it look like a successful run at first glace.
    --2019-01-21 12:07:16--  https://composer.github.io/installer.sig
    Resolving composer.github.io (composer.github.io)... 185.199.111.153, 185.199.108.153, 185.199.109.153, ...
    Connecting to composer.github.io (composer.github.io)|185.199.111.153|:443... failed: Connection refused.
    Connecting to composer.github.io (composer.github.io)|185.199.108.153|:443... failed: Connection refused.
    Connecting to composer.github.io (composer.github.io)|185.199.109.153|:443... failed: Connection refused.
    Connecting to composer.github.io (composer.github.io)|185.199.110.153|:443... failed: Connection refused.

--- lib/helpers/setup_composer.sh
+++ lib/helpers/setup_composer.sh
@@ -1,7 +1,7 @@
 # require utils/messages.sh
 # require utils/errors.sh
 composer_check_signature() {
-	EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+	EXPECTED_SIGNATURE=$(wget -O - https://composer.github.io/installer.sig)
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
