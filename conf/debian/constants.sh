52e7834d66ad02cfa5a24d669fe4a600c35e802a
Diego Lendoiro <diego@passbolt.com>
2019-08-06 16:36:34 +0200

    Changed: fixed package versions for debian10

--- conf/debian/constants.sh
+++ conf/debian/constants.sh
@@ -1,8 +1,8 @@
 readonly OS='debian'
-readonly OS_SUPPORTED_VERSION="9.0"
+readonly OS_SUPPORTED_VERSION="10.0"
 readonly OS_VERSION_FILE="/etc/debian_version"
-readonly FPM_WWW_POOL="/etc/php/7.0/fpm/pool.d/www.conf"
-readonly FPM_SERVICE="php7.0-fpm"
+readonly FPM_WWW_POOL="/etc/php/7.3/fpm/pool.d/www.conf"
+readonly FPM_SERVICE="php7.3-fpm"
 readonly WWW_USER="www-data"
 readonly WWW_USER_HOME="/home/www-data"
 readonly GNUPG_HOME='/home/www-data/.gnupg'
