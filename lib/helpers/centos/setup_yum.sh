f1bfca92bb4c39364abdac49b7f813a8caca525a
TheDeadGuy <36852974+TheDeadGuy@users.noreply.github.com>
2021-02-03 16:43:23 +0000

    Update setup_yum.sh

--- lib/helpers/centos/setup_yum.sh
+++ lib/helpers/centos/setup_yum.sh
@@ -3,19 +3,20 @@ setup_yum() {
   case $os in
     centos)
       install_packages "yum-utils epel-release $REMI_PHP_URL"
-      yum-config-manager --enable "$REMI_PHP_VERSION"
+      yum-config-manager --enable remi powertools baseos 
+      dnf module enable php:"$REMI_PHP_VERSION"
       ;;
     redhat)
       enable_repos
       if ! yum list installed | grep epel-release; then
-        install_packages "https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
+        install_packages "https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
       fi
   esac
 }
 
 
 enable_repos() {
-  local repos=(rhel-server-rhscl-7-rpms rhel-7-server-extras-rpms rhel-7-server-optional-rpms)
+  local repos=(rhel-server-rhscl-8-rpms rhel-8-server-extras-rpms rhel-8-server-optional-rpms)
   local enabled_repos=""
 
   enabled_repos="$(subscription-manager repos --list-enabled | grep 'Repo ID' | awk '{print $3}')"
